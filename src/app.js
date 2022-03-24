a2f5ee5f4da24af7a150f4e68c0ab4a30dc76e64
Radwane69 <rnougha@edenschool.fr>
2022-03-24 15:36:28 +0100

    commit content

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import {
+    name
+} from 'commander';
+import {
+    v4 as uuidv4
+} from 'uuid';
+
+
+let hobbies = [Cinéma, Musique, Photographie, Série, voyage, cuisine, randonnée, lecture, jeux - vidéo, foot]
+let user = [];
+for (let i = 0; i < 1000; i++) {
+    let id = uuidv4();
+    user.push({
+        id: id
+        hobbies: hobbie[Math.floor(Math.random() * hobbies.length)]
+    });
+}
+console.log(user);
