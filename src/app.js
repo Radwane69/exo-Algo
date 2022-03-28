d3868d430a9b39cc4696173067a622a49c1e2dce
Radwane69 <rnougha@edenschool.fr>
2022-03-28 15:55:39 +0200

    commit content

--- src/app.js
+++ src/app.js
@@ -5,14 +5,13 @@ import {
     v4 as uuidv4
 } from 'uuid';
 
+let hobbies = [{id: 1, name: 'Sport'}, {id: 2, name:'Cinéma'}, {id: 3, name: 'Voyage'}, {id: 4, name: 'Film'}, {id: 5, name: 'Jeux vidéo'}, {id: 6, name: 'Musculation'}, {id: 7, name: 'Jardinage'}, {id: 8, name: 'Peinture'}, {id: 9, name: 'Décoration intérieur'}, {id: 10, name: 'Jeux de société'},]
 
-let hobbies = [Cinéma, Musique, Photographie, Série, voyage, cuisine, randonnée, lecture, jeuxvidéo, foot]
 let user = [];
 for (let i = 0; i < 1000; i++) {
     let id = uuidv4();
     user.push({
         id: id
-        hobbies: hobbie[Math.floor(Math.random() * hobbies.length)]
     });
 }
 console.log(user);
